5f1b111ae038eb9cc0a8d4d6b902dda88b0d4356
Alvis Low <alvis@vilota.ai>
2024-07-10 13:37:47 +0800

    Add changes to GPMF_demo.h

--- demo/GPMF_demo.h
+++ demo/GPMF_demo.h
@@ -2,11 +2,24 @@
 #define GPMF_DEMO_H
 
 #include <stdint.h> 
-#include "../GPMF_parser.h"
+#include "../GPMF_parser.h" 
+#include "GPMF_mp4reader.h"
+#include "../GPMF_utils.h"  
 
-extern int read_mp4(char* mp4Directory, char* flag_f);
-extern char* CorruptTheMP4(char* filename);
-extern GPMF_ERR readMP4File(char* filename);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void openOutputFile(const char* filename);
+void closeOutputFile(void);
+void writeToFile(const char* format, ...);
+int read_mp4(const char* mp4Directory, const char* flag_f);
+char* CorruptTheMP4(const char* filename);
+GPMF_ERR readMP4File(const char* filename);
 extern void PrintGPMF(GPMF_stream* ms);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif 
